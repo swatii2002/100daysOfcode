ab4b952bb1fdeaf6e1e04d854f14fb2a656c2910
swati <89398578+singhswatii@users.noreply.github.com>
2023-07-01 22:40:53 +0530

    Create day25.cpp
    1859. Sorting the Sentence
    Easy
    
    A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
    
    A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
    
    For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
    Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
    
     
    
    Example 1:
    
    Input: s = "is2 sentence4 This1 a3"
    Output: "This is a sentence"
    Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
    Example 2:
    
    Input: s = "Myself2 Me1 I4 and3"
    Output: "Me Myself and I"
    Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
     
    
    Constraints:
    
    2 <= s.length <= 200
    s consists of lowercase and uppercase English letters, spaces, and digits from 1 to 9.
    The number of words in s is between 1 and 9.
    The words in s are separated by a single space.
    s contains no leading or trailing spaces.

---
+++ day25.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+                string result;
+      vector<pair<int,string>>p;
+      istringstream ss(s);
+      string word;
+      while(ss >> word){
+          int index=word[word.length()-1];
+          word.pop_back();  
+          p.push_back({index,word});
+      }
+
+      sort(p.begin(),p.end());
+
+      for(auto it:p){
+          result +=it.second;
+          result+=' ';    
+      }
+      result.pop_back();
+      return result;
+
+    }
+};
